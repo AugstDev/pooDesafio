f4ba46c6855df92fb9fcd6d036ed240df3d5f8b3
AugstDev <igoraugsa@gmail.com>
2024-07-26 15:00:51 -0300

    messages

--- pooDesafio/src/pooDesafio/Iphone.java
+++ pooDesafio/src/pooDesafio/Iphone.java
@@ -4,55 +4,55 @@ public class Iphone implements AparelhoEletronico, NavegadorInternet, Reprodutor
 
 	@Override
 	public void tocar() {
-		//lógica para tocar
+		//lógica para tocar.
 		
 	}
 
 	@Override
 	public void pausar() {
-		//lógica para pausar
+		//lógica para pausar.
 		
 	}
 
 	@Override
 	public void selecionarMusica(String musica) {
-		//lógica para selecionarMusica
+		//lógica para selecionarMusica.
 		
 	}
 
 	@Override
 	public void exibirPagina(String url) {
-		//lógica para exibir url
+		//lógica para exibir url.
 		
 	}
 
 	@Override
 	public void adicionarNovaAba() {
-		//lógica para adicionarNovaAba
+		//lógica para adicionarNovaAba.
 		
 	}
 
 	@Override
 	public void atualizarPagina() {
-		//lógica para atualizarPagina
+		//lógica para atualizarPagina.
 		
 	}
 
 	@Override
 	public void ligar(String numero) {
-		//lógica para ligar para o numero
+		//lógica para ligar para o numero.
 		
 	}
 
 	@Override
 	public void atender() {
-		//lógica para atender
+		//lógica para atender.
 		
 	}
 
 	@Override
 	public void iniciarCorreioVoz() {
-		//lógica para iniciarCorreioVoz
+		//lógica para iniciarCorreioVoz.
 		
 	}
 	
